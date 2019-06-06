26087ffa9c9795e4f96f353b7aed08599f1f18bc
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-06-06 14:45:18 +0200

    Add files via upload

---
+++ D.DI/D.DI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+
+namespace D.DI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"C:\temp|folder1");
+
+            // var files = Directory.GetFiles("...");
+            //foreach(var file in files)
+            //Console.WriteLine(files);
+
+            var directories = Directory.GetDirectories("...", "*.*", SearchOption.AllDirectories);
+            foreach(var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+            var directoryinfo = new DirectoryInfo("...");
+            directoryinfo.GetFiles();
+            directoryinfo.GetDirectories();
+        }
+    }
+}
---
+++ PH/PH/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace PH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "...";
+            var dotindex = path.IndexOf('.');
+            var extension = path.Substring(dotindex);
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("file name: "+ Path.GetFileName(path));
+            Console.WriteLine("file name without Extension: "+Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("DIrectory name:"+Path.GetDirectoryName(path));
+        }
+    }
+}
